2ebfa9e89e78a9f6d45305e452eb8a6b1312da65
Yeseul Kim <borisu0815@gmail.com>
2017-08-25 17:58:35 +0800

    Create Pubmed.py

---
+++ Pubmed.py
@@ -0,0 +1,8 @@
+fetch = PubMedFetcher()
+ * You can also use fetch = PubMedFetcher(cachedir='/path/to/cachedir') to specify the cache. 
+article = fetch.article_by_pmid('123456')
+print(article.title)
+print(article.journal, article.year, article.volume, article.issue)
+print(article.authors)
+
+
